c8fcacf3b4331af8380c2ecf7340612b7f1b98d5
Shashi Bhagat <skujur871@gmail.com>
2021-09-18 02:10:03 +0530

    data types in python

---
+++ app.py
@@ -0,0 +1,19 @@
+import math
+print('Hello Shashi')
+
+print('*' * 12)
+
+# formated stirng
+
+str = f"Shashi is learning pyhton"
+
+print(str)
+print(len(str))
+print(str.capitalize())
+print(str.upper())
+print(str.find('S'))
+print('Sh' in str)
+print(str.replace('Shashi', "Shashi Bhushan"))
+
+print(round(2.9))
+print(math.ceil(2.2))
