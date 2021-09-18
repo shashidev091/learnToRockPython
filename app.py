5fd7634b408b6cd6e4b99ab04a2c1b1ff62ec9a0
Shashi Bhagat <skujur871@gmail.com>
2021-09-18 18:31:02 +0530

    conditional programming in python

--- app.py
+++ app.py
@@ -20,3 +20,23 @@ print(math.ceil(2.2))
 print(bool(5))
 print(bool(0))
 print(bool("False"))
+
+temperature = 10
+
+if temperature < 5:
+    print('Temperature is low')
+    print(f'Temperature is below {temperature}')
+elif temperature == 10:
+    print(f'Temperature is exactly same as {temperature}')
+else:
+    print("amazing")
+
+# Ternary Operator in python
+# and or not => These are logical operators
+a = 11
+mini = a if a > 10 else 0
+
+print(mini)
+
+b = input('Enter something\n')
+print(b)
