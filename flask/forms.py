008d9895eeb500c16fd477d8b55fe2e74e165cd9
Shashi Bhagat <skujur871@gmail.com>
2021-10-04 23:22:46 +0530

    Added card for options

--- flask/forms.py
+++ flask/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -9,3 +9,11 @@ class RegisterForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember = BooleanField('remember me')
+    submit = SubmitField('Login')
+
