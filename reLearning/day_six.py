d6bac3560e9074d97b6f81d7dcef752f12e4093b
Shashi Bhagat <skujur871@gmail.com>
2022-01-23 20:19:11 +0530

    tutorial day 9 cypher text

--- reLearning/day_six.py
+++ reLearning/day_six.py
@@ -8,13 +8,27 @@ def encode_msg():
     alphabets = list('abcdefghijklmnopqrstuvwxyz')
 
     cypher_type = input("Enter encode or decode\n")
-    number = input('Enter no. to split')
+    number = int(input('Enter no. to split \n'))
+    msg = input('Input message \n')
 
-    msg = input('Input message')
-
-    for item, index in enumerate(msg):
-        msg[index] = ''
-
+    if cypher_type == 'encode':
+        for index, item in enumerate(msg):
+            idx = alphabets.index(item) + number
+            if idx > len(alphabets):
+                idx -= 26
+                print(idx)
+            msg = msg.replace(item, alphabets[idx])
+    elif cypher_type == 'decode':
+        for index, item in enumerate(msg):
+            idx = alphabets.index(item) - number
+            if idx > len(alphabets):
+                idx += 26
+                print(idx)
+            msg = msg.replace(item, alphabets[idx])
+    else:
+        print("wrong input")
+        return
+    print(msg)
 
 
 encode_msg()
