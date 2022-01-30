cb94e9536293ae00a29aa2e920d852452db2216c
Shashi Bhagat <skujur871@gmail.com>
2022-01-30 23:29:13 +0530

    tutorial day 12 functions

--- reLearning/day_10.py
+++ reLearning/day_10.py
@@ -35,18 +35,32 @@ index = 0
 def find_biggest_bid():
     name = input('Enter your name \n')
     bid_amount = int(input('Enter the bid amount \n'))
-    bid_list = [name, bid_amount]
     global index
     index += 1
-    all_bid[f'Person {index}'] = bid_list
+    all_bid[name] = bid_amount
     print(all_bid)
 
-    isMoreEntry = input('Do you want to enter more bids')
+    is_more_entry = input('Do you want to enter more bids \n')
 
-    if isMoreEntry == 'yes':
+    if is_more_entry == 'yes':
         find_biggest_bid()
     else:
-        print(all_bid)
+        list_of_bids = 0
+        winner = ''
+        for bid in all_bid:
+            if all_bid[bid] > list_of_bids:
+                list_of_bids = all_bid[bid]
+                winner = bid
+
+        print(f'The highest bid is {list_of_bids} by {winner}')
 
 
-find_biggest_bid()
+# find_biggest_bid()
+
+
+def formate_name(f_name, l_name):
+    return f"{f_name.title()} {l_name.title()}"
+
+
+# print(formate_name("SHASHI", "Bhagat"))
+
