20c9a8762247020cd69044198ef6b565b691cf4e
Shashi Bhagat <skujur871@gmail.com>
2022-02-09 23:55:38 +0530

    changing colors in each iteration

--- reLearning/Inremediate_python/main.py
+++ reLearning/Inremediate_python/main.py
@@ -1,9 +1,12 @@
+import random
 from turtle import Turtle, Screen
 
 my_turtle = Turtle()
 my_turtle.shape('turtle')
 
 # temp_turtle = Turtle()
+
+colors = ["CornFlowerBlue", "DarkOrchid", "IndianRed", "SlateGray", "SeaGreen"]
 
 
 def draw_square():
@@ -27,5 +30,15 @@ def draw_dashed_line():
     screen.exitonclick()
 
 
-draw_dashed_line()
+# draw_dashed_line()
 
+def generic_shape(no_sides):
+    for _ in range(no_sides):
+        angle = 360 / no_sides
+        my_turtle.forward(100)
+        my_turtle.right(angle)
+
+
+for shape_side_no in range(3, 11):
+    my_turtle.color(random.choice(colors))
+    generic_shape(shape_side_no)
