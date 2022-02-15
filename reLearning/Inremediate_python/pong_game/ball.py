fcecf358602dcd0ea1a6d68db92839aa8936465b
Shashi Bhagat <skujur871@gmail.com>
2022-02-16 04:10:49 +0530

    Pong Game => added collision, bounce and paddle

--- reLearning/Inremediate_python/pong_game/ball.py
+++ reLearning/Inremediate_python/pong_game/ball.py
@@ -5,4 +5,24 @@ class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.create_ball()
+        self.x_cors = 1
+        self.y_cors = 1
+
+    def create_ball(self):
+        self.shape('circle')
+        self.penup()
+        self.color('salmon')
+        # self.goto(0, 0)
+        # self.setheading(30)
+
+    def move(self):
+        x_cors = self.xcor() + self.x_cors
+        y_cors = self.ycor() + self.y_cors
+        self.goto(x_cors, y_cors)
+
+    def bounce_x(self):
+        self.x_cors *= -1
+
+    def bounce_y(self):
+        self.y_cors *= -1
--- reLearning/Inremediate_python/pong_game/main.py
+++ reLearning/Inremediate_python/pong_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 pong_screen = Screen()
 pong_screen.setup(width=800, height=600)
@@ -8,6 +9,7 @@ pong_screen.bgcolor('#000')
 pong_screen.tracer(0)
 l_paddle = Paddle((350, 0))
 r_paddle = Paddle((-350, 0))
+ball = Ball()
 game_is_on = True
 
 
@@ -27,7 +29,23 @@ pong_screen.onkey(close_app, 'p')
 
 
 while game_is_on:
+    # if ball.xcor() >= 230 or ball.ycor() > 230:
+    #     ball.setheading(ball.heading() - 270)
+    # if ball.xcor() <= -230 or ball.ycor() < -230:
+    #     ball.setheading(ball.heading() + 270)
+    # ball.forward(1)
     pong_screen.update()
+    ball.move()
+
+    # wall bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() > 330 or ball.distance(r_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+
+    if ball.xcor() > 360 or ball.xcor() < -360:
+        close_app()
 
 
 pong_screen.exitonclick()
