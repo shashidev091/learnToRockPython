c5321751697c99118f57d97d79baa95f188beb49
Shashi Bhagat <skujur871@gmail.com>
2022-03-01 00:15:38 +0530

    list comprehension

--- reLearning/Inremediate_python/list_comprehension.py
+++ reLearning/Inremediate_python/list_comprehension.py
@@ -1,3 +1,5 @@
+import random
+
 list_numbers = [1, 3, 5, 6, 78, 3, 5, 55]
 new_list = [n + 10 for n in list_numbers]
 print(new_list)
@@ -8,3 +10,10 @@ print(new_names)
 
 new_range_value = [item * 2 for item in range(1, 5)]
 print(new_range_value)
+
+list_of_random_numbers = [random.randint(1, 100) for number in range(1, 10)]
+print(list_of_random_numbers)
+
+prime_numbers = [prime_number for prime_number in list_of_random_numbers if prime_number % 2 == 0]
+print(prime_numbers)
+
