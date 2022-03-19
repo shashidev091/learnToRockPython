c7a276ac2ca33ce1339fcb7f9b5c9b13ad782d5b
Shashi Bhagat <skujur871@gmail.com>
2022-03-19 22:16:59 +0530

    Email Service using python 2

--- reLearning/Inremediate_python/email_service/date_time.py
+++ reLearning/Inremediate_python/email_service/date_time.py
@@ -1,4 +1,9 @@
 import datetime as dt
+import random
+import smtplib
+
+MY_EMAIL = 'shashibhushanbhagat@yahoo.com'
+MY_APP_PASSWORD = 'eqiaeujqwxhsdlyd'
 
 now = dt.datetime.now()
 print(now.year)
@@ -7,6 +12,16 @@ year = now.year
 if year == 2022:
     print("Still wear mask")
 
-with open('quotes.txt', 'r') as data_file:
-    data = data_file.readlines()
-    print(data)
+if now.weekday() == 5:
+    with open('quotes.txt', 'r') as data_file:
+        data = data_file.readlines()
+        print(data)
+
+        with smtplib.SMTP(host="smtp.mail.yahoo.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_APP_PASSWORD)
+            connection.sendmail(to_addrs="skujur873@gmail.com", from_addr=MY_EMAIL,
+                                msg=f"Subject:Monday Motivation\n\n {random.choice(data)}")
+
+print(now.weekday())
+print(now.day)
