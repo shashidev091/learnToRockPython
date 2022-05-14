c0330a5c66d47acc3c6c3b7c9cb3c397bd06ad45
Shashi Bhagat <skujur871@gmail.com>
2022-05-15 00:00:55 +0530

    navin reddy day 3

--- reLearning/dataStructures/second.py
+++ reLearning/dataStructures/second.py
@@ -7,3 +7,14 @@ values = array([[1, 2, 4, 5, 6], [1, 34, 5, 4, 9]])
 
 for item in values:
     print(item)
+
+
+def add_sub(x, y):
+    adds = x + y
+    sub = x - y
+    return adds, sub
+
+
+result_add, result_sub = add_sub(10, 20)
+
+print(result_add, result_sub)
