33123a18ef87923398f74daa18cc2ab9457b93df
Shashi Bhagat <skujur871@gmail.com>
2022-06-06 02:27:18 +0530

    new migrations

--- Django/store/models.py
+++ Django/store/models.py
@@ -16,6 +16,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -58,6 +59,7 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.PositiveBigIntegerField(max_length=6, null=True)
     # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
