4ba7d101469729b3c9f58dbb7a1c3f2493384434
Shashi Bhagat <skujur871@gmail.com>
2022-06-10 04:16:58 +0530

    configured apis and created html page

--- Django_youtube/blogger_shashi/blogger_shashi/settings.py
+++ Django_youtube/blogger_shashi/blogger_shashi/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My manual additions
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
