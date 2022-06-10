357b21c2f2023f6dbfa9b3ba4afc4cbd518dd7fc
Shashi Bhagat <skujur871@gmail.com>
2022-06-11 01:58:43 +0530

    created navbar

--- Django_youtube/blogger_shashi/blogs/views.py
+++ Django_youtube/blogger_shashi/blogs/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+personal_info = {
+    "name": "Shashi Bhagat",
+    "mobile_no": "9931920057"
+}
 # Create your views here.
+
+
 def landing_page(request):
     return render(request, 'index.html')
 
+
 def about_page(request):
-    return HttpResponse('Hi! I am Shashi Bhushan Bhagat')
+    # return HttpResponse('Hi! I am Shashi Bhushan Bhagat')
+    return render(request, 'aboutUs.html', personal_info)
+
 
 def services_page(request):
     return HttpResponse('<b>We have several sevices, you just have to ask what</b>')
