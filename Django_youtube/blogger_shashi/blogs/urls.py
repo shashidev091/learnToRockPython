85120aed0984a04bd80ad9eec05805f256b8dc8c
Shashi Bhagat <skujur871@gmail.com>
2022-06-12 19:44:16 +0530

    fetch country codes

--- Django_youtube/blogger_shashi/blogs/urls.py
+++ Django_youtube/blogger_shashi/blogs/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.landing_page, name='home'),
     path('about-us', views.about_page, name='about'),
     path('services', views.services_page, name='services'),
-    path('blog', views.blog, name='blog')
+    path('blog', views.blog, name='blog'),
+    path('<str:info>', views.fetch_info),
+    path('country/<str:country>', views.find_country_code)
 ]
